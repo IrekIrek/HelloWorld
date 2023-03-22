0bf3160c79b609f86c552f72582511756cdea5e1
irek <irek@LENOVO_C470>
2023-03-22 20:05:11 +0100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string imie = "Irek";
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Druga linijka kodu");
+            double a = 20;
+            double b = 10;
+            
+            Math math = new Math();
+            
+            double addResult = math.Add(a, b);
+            double substractResult = math.Subtract(a, b);
+            
+            Console.WriteLine(addResult.ToString());
+            Console.WriteLine(substractResult.ToString());
+
+        }
+    }
+}
